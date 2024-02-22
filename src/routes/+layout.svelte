a112741e9aecc942439bc9782628b3aa4059eba5
Bryan Keane <bryan.keane@genesys.com>
2024-02-22 18:07:14 +0000

    Added preliminary sidebar menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,48 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	// @ts-ignore - unsure why this is needed but will fix later
 	import { Button } from '$lib/components/ui/button';
-	import Sun from 'lucide-svelte/icons/sun';
-	import Moon from 'lucide-svelte/icons/moon';
+	import { cn } from '$lib/utils.js';
 
-	import { toggleMode } from 'mode-watcher';
+	let className: string | null | undefined = undefined;
+	export { className as class };
 </script>
 
 <ModeWatcher />
-<slot />
 
-<Button on:click={toggleMode} variant="outline" size="icon">
-	<Sun
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Moon
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
-	<span class="sr-only">Toggle theme</span>
-</Button>
+<div class="hidden md:block min-h-screen">
+  <div class="border-t">
+    <div class="bg-background">
+      <div class="grid lg:grid-cols-5">
+        <div class={cn('pb-12 border-r', className)}>
+          <div class="space-y-4 py-4">
+            <div class="px-3 py-2">
+              <h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Library</h2>
+              <div class="space-y-1">
+                <Button variant="secondary" class="w-full justify-start">Programs</Button>
+                <Button variant="ghost" class="w-full justify-start">Workouts</Button>
+                <Button variant="ghost" class="w-full justify-start">Radio</Button>
+              </div>
+            </div>
+			<div class="px-3 py-2">
+				<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">People</h2>
+				<div class="space-y-1">
+				  <Button variant="secondary" class="w-full justify-start">Clients</Button>
+				  <Button variant="ghost" class="w-full justify-start">Messaging</Button>
+				</div>
+			  </div>
+          </div>
+        </div>
+
+        <div class="relative flex min-h-screen flex-col">
+          <slot />
+        </div>
+
+        <div class="col-span-3 lg:col-span-4 lg:border-l"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="overflow-hidden rounded-[0.5rem] border bg-background shadow-xl"></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+i am the cool slot
