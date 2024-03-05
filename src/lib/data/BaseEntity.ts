8664b433907b958e812fd8c31d58a39482cd8d40
Bryan Keane <bryan.keane@genesys.com>
2024-03-05 08:46:34 +0000

    8: Migrated data models from old project

---
+++ src/lib/data/BaseEntity.ts
@@ -0,0 +1,25 @@
+export abstract class BaseEntityTemplate {
+	uid: string;
+	name: string;
+	description: string;
+
+	constructor(uid: string, name: string, description: string) {
+		this.uid = uid;
+		this.name = name;
+		this.description = description;
+	}
+}
+
+export abstract class BaseEntityInstance extends BaseEntityTemplate {
+	templateUid: string;
+
+	constructor(
+		uid: string,
+		name: string,
+		description: string,
+		templateUid: string
+	) {
+		super(uid, name, description);
+		this.templateUid = templateUid;
+	}
+}
