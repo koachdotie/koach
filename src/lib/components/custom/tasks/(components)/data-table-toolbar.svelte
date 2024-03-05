a75ce3318ca14579575859ba921246bd471eed1b
Bryan Keane <bryan.keane@genesys.com>
2024-03-05 08:59:48 +0000

    8: Added data table from shadcn svelte example

---
+++ src/lib/components/custom/tasks/(components)/data-table-toolbar.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Input } from "@/registry/new-york/ui/input";
+	import { DataTableFacetedFilter, DataTableViewOptions } from ".";
+	import type { Task } from "../(data)/schemas";
+	import type { TableViewModel } from "svelte-headless-table";
+	import Button from "@/registry/new-york/ui/button/button.svelte";
+	import Cross2 from "svelte-radix/Cross2.svelte";
+	import { statuses, priorities } from "../(data)/data";
+	import type { Writable } from "svelte/store";
+
+	export let tableModel: TableViewModel<Task>;
+
+	const { pluginStates } = tableModel;
+	const {
+		filterValue,
+	}: {
+		filterValue: Writable<string>;
+	} = pluginStates.filter;
+
+	const {
+		filterValues,
+	}: {
+		filterValues: Writable<{
+			status: string[];
+			priority: string[];
+		}>;
+	} = pluginStates.colFilter;
+
+	$: showReset = Object.values({ ...$filterValues, $filterValue }).some((v) => v.length > 0);
+</script>
+
+<div class="flex items-center justify-between">
+	<div class="flex flex-1 items-center space-x-2">
+		<Input
+			placeholder="Filter tasks..."
+			class="h-8 w-[150px] lg:w-[250px]"
+			type="search"
+			bind:value={$filterValue}
+		/>
+
+		<DataTableFacetedFilter
+			bind:filterValues={$filterValues.status}
+			title="Status"
+			options={statuses}
+		/>
+		<DataTableFacetedFilter
+			bind:filterValues={$filterValues.priority}
+			title="Priority"
+			options={priorities}
+		/>
+		{#if showReset}
+			<Button
+				on:click={() => {
+					$filterValue = "";
+					$filterValues.status = [];
+					$filterValues.priority = [];
+				}}
+				variant="ghost"
+				class="h-8 px-2 lg:px-3"
+			>
+				Reset
+				<Cross2 class="ml-2 h-4 w-4" />
+			</Button>
+		{/if}
+	</div>
+
+	<DataTableViewOptions {tableModel} />
+</div>
