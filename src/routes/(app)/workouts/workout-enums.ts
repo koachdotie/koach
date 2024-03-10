497a6424b1f662fecccb58d0105e212ef3dc7cbb
Bryan Keane <bryan.keane@genesys.com>
2024-03-10 16:57:51 +0000

    11: Added generic table to workouts

---
+++ src/routes/(app)/workouts/workout-enums.ts
@@ -0,0 +1,57 @@
+import type { EnumValueScheme } from "$lib/components/custom/generic-datatable/generic-cell.svelte";
+import { Dumbbell, PersonStanding, Blocks, Trophy, ArrowDown, Activity } from "lucide-svelte";
+
+export const potentialValues: Map<string, EnumValueScheme[]> = new Map([
+    [
+        'modality',
+        [
+            {
+                value: 'hypertrophy',
+                label: 'Hyperthrophy',
+                icon: Dumbbell
+            },
+            {
+                value: 'strength',
+                label: 'Strength',
+                icon: PersonStanding
+            },
+            {
+                value: 'powerbuilding',
+                label: 'Powerbuilding',
+                icon: Blocks
+            },
+            {
+                value: 'sportspecific',
+                label: 'Sport-Specific',
+                icon: Trophy
+            },
+            {
+                value: 'power',
+                label: 'Power',
+                icon: ArrowDown
+            },
+            {
+                value: 'cardiovascular',
+                label: 'Cardiovascular',
+                icon: Activity
+            }
+        ]
+    ],
+    [
+        'experienceLevel',
+        [
+            {
+                label: 'Beginner',
+                value: 'beginner'
+            },
+            {
+                label: 'Intermediate',
+                value: 'intermediate'
+            },
+            {
+                label: 'Advanced',
+                value: 'advanced'
+            }
+        ]
+    ]
+]);
